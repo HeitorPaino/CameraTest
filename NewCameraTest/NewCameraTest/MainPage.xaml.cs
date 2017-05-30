ab7be4e880f8897f6bbc114d8980e8e146546a2b
Heitor Paino Aoun <heitor.paino@metasix.com.br>
2017-05-29 21:36:09 -0300

    first commit

---
+++ NewCameraTest/NewCameraTest/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace NewCameraTest
+{
+	public partial class MainPage : ContentPage
+	{
+        public ICommand TirarFotoCommand { get; set; }
+        public ImageSource foto = "foto.png"; 
+        public ImageSource Foto {
+            get
+            {
+                return foto;
+            }
+            set
+            {
+                foto = value;
+                OnPropertyChanged();
+            }
+        }        
+
+        public MainPage()
+		{
+			InitializeComponent();
+            this.TirarFotoCommand = new Command(() =>
+            {
+                DependencyService.Get<ICamera>().tirarFoto();
+            });
+            MessagingCenter.Subscribe<byte[]>(this,"FotoTirada",
+                (bytes) =>
+                {
+                    Foto = ImageSource.FromStream(
+                        () => new MemoryStream(bytes));
+                });
+            this.BindingContext = this;
+        }
+	}
+}
